c1380b2c3fa2b71b435b1ed6376fa452afe0c8bf
Herve <dockerlite@gmail.com>
2023-03-19 16:05:44 +0000

    adding git sample

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@ node default {
     content  => "Hello world\n",
   }
   notify { 'after': }
-  include git_resource
+  include git
   git { 'git_latest':
     path   => '/tmp/git_latest',
     ensure => present,
